82a29930eeca5db171f60fee0b92ef78c77674dc
zendy <nguyenduc.051194@gmail.com>
2023-03-19 13:33:09 +0700

    Restricting tasks

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -12,10 +12,11 @@ export class TasksRepository extends Repository<Task> {
     super(Task, dataSource.createEntityManager());
   }
 
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
 
     const query = this.createQueryBuilder('task');
+    query.where({ user });
 
     if (status) {
       query.where('task.status = :status', { status });
@@ -23,7 +24,7 @@ export class TasksRepository extends Repository<Task> {
 
     if (search) {
       query.where(
-        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
         { search: `%${search}%` },
       );
     }
